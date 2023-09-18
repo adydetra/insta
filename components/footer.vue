5fa87a352e4ec2edf86cbbb60c17e0d12cc1e9f7
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-18 17:33:28 +0700

    chore: better border bottom

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="font-mono text-xs md:text-sm lg:text-xs 2xl:text-sm text-gray-400 dark:text-gray-600 space-y-2 px-3">
+  <footer class="font-mono text-xs md:text-sm lg:text-xs 2xl:text-sm text-gray-400 space-y-2 px-3">
     <p>
       by <NuxtLink to="https://github.com/devdewa" target="_blank" title="devdewa" :class="link">@devdewa</NuxtLink>, appreciate my work on
       <NuxtLink to="https://github.com/sponsors/devdewa" target="_blank" title="Github Sponsors" :class="link">Sponsors</NuxtLink>
@@ -12,5 +12,5 @@
 </template>
 
 <script lang="ts" setup>
-const link = "border-b";
+const link = "border-b dark:border-gray-800 pb-0.5";
 </script>
