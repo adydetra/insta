40324189527c03398506b39825edd73f0191f41c
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-19 11:42:26 +0700

    chore: minimalist icon

--- components/color.vue
+++ components/color.vue
@@ -1,12 +1,14 @@
 <template>
   <button aria-label="Color Mode" @click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')">
-    <Icon v-if="$colorMode.value == 'dark'" name="line-md:circle-twotone" class="w-4 h-4 lg:w-6 lg:h-6" />
-    <Icon v-else name="line-md:circle" class="w-4 h-4 lg:w-6 lg:h-6" />
+    <Icon v-if="$colorMode.value == 'dark'" name="line-md:circle-twotone" :class="icon" />
+    <Icon v-else name="line-md:circle" :class="icon" />
   </button>
 </template>
 
 <script setup lang="ts">
 type Theme = "light" | "dark";
+
+const icon = "w-4 h-4 md:w-5 md:h-5 mt-2"
 
 const setColorTheme = (newTheme: Theme) => {
   useColorMode().preference = newTheme;
