365a00ca43a6a3b6bb227a5519923d9658a69bea
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-30 09:24:54 +0700

    chore: rename devdewa to adydetra

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="font-mono text-xs md:text-sm lg:text-xs 2xl:text-sm text-gray-400 space-y-2 px-3">
     <p>
-      by <NuxtLink to="https://github.com/devdewa" target="_blank" title="devdewa" :class="link">@devdewa</NuxtLink>, appreciate my work on
-      <NuxtLink to="https://github.com/sponsors/devdewa" target="_blank" title="Github Sponsors" :class="link">Sponsors</NuxtLink>
+      by <NuxtLink to="https://github.com/adydetra" target="_blank" title="adydetra" :class="link">@adydetra</NuxtLink>, appreciate my work on
+      <NuxtLink to="https://github.com/sponsors/adydetra" target="_blank" title="Github Sponsors" :class="link">Sponsors</NuxtLink>
     </p>
     <p>
       powered by nuxt 💚. source on
-      <NuxtLink to="https://github.com/devdewa/insta" target="_blank" title="Github" :class="link"><Icon name="line-md:github-loop" title="Github" /></NuxtLink>
+      <NuxtLink to="https://github.com/adydetra/insta" target="_blank" title="Github" :class="link"><Icon name="line-md:github-loop" title="Github" /></NuxtLink>
     </p>
   </footer>
 </template>
