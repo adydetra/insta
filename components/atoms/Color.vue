4f8348cadde93fd704704e4bbf8762cbc1798e37
devdewa <uchiasasuke123.sp@gmail.com>
2023-11-04 20:57:00 +0700

    chore(Color): revision title, add condition

--- components/atoms/Color.vue
+++ components/atoms/Color.vue
@@ -1,5 +1,10 @@
 <template>
-  <button aria-label="Color Mode" @click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')" title="Color Mode" class="hover:opacity-80">
+  <button
+    aria-label="Color Mode"
+    @click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')"
+    :title="$colorMode.preference == 'dark' ? 'Switch to light' : 'Switch to dark'"
+    class="hover:opacity-80"
+  >
     <Icon v-if="$colorMode.value == 'dark'" name="line-md:sunny-outline-to-moon-alt-loop-transition" :class="icon" />
     <Icon v-else name="line-md:sunny-outline-loop" :class="icon" />
   </button>
@@ -8,7 +13,7 @@
 <script setup lang="ts">
 type Theme = "light" | "dark";
 
-const icon = "w-4 h-4 md:w-5 md:h-5 lg:w-4 lg:h-4 2xl:w-5 2xl:h-5 mt-2"
+const icon = "w-4 h-4 md:w-5 md:h-5 lg:w-4 lg:h-4 2xl:w-5 2xl:h-5 mt-2";
 
 const setColorTheme = (newTheme: Theme) => {
   useColorMode().preference = newTheme;
