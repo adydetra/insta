69f597b6571dcd60e784ac1279855c06817cbbbd
adydetra <devdewa123.sp@gmail.com>
2024-11-22 14:19:41 +0700

    chore: turn on PWA config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,36 +11,36 @@ export default defineNuxtConfig({
 
   modules: ['@unocss/nuxt', '@nuxtjs/color-mode', '@nuxt/image', '@vueuse/nuxt', '@vite-pwa/nuxt'],
 
-  // pwa: {
-  //   registerType: 'autoUpdate',
-  //   manifest: {
-  //     name: 'insta',
-  //     short_name: 'insta',
-  //     theme_color: '#FFF',
-  //     background_color: '#FFF',
-  //     icons: [
-  //       {
-  //         src: 'pwa-64x64.png',
-  //         sizes: '64x64',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: 'pwa-192x192.png',
-  //         sizes: '192x192',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: 'pwa-512x512.png',
-  //         sizes: '512x512',
-  //         type: 'image/png',
-  //       },
-  //     ],
-  //   },
-  //   devOptions: {
-  //     enabled: true,
-  //     type: 'module',
-  //   },
-  // },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'insta',
+      short_name: 'insta',
+      theme_color: '#FFF',
+      background_color: '#FFF',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+    },
+  },
 
   image: {
     quality: 1,
